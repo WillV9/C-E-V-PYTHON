733330a783ba6eca357ac9e2d902cb419b285d49
Willian Viana <46612336+WillV9@users.noreply.github.com>
2020-03-24 23:44:58 -0700

    ex15.py

--- Ex15.py
+++ Ex15.py
@@ -1,24 +1,29 @@
-# cook your dish here
-#               desafio 99
+#                   DESAFIO 99
+#   FACA UM PROGRAMA QUE TENHA UMA FUNCAO CHAMADA MAIOR(), QUE RECEBA VARIOS PARAMETROS COM VALORES INTEIROS.
+#   SEU PROGRAMA TEM QUE ANALISAR TODOS OS VALORES E DIZER QUAL E MAIOR
 from time import sleep
+from random import randint
 
 
 def bigger(* num):
     cont = bigger = 0
-    print('-='*3 , "Analizing Order of Numbers", '=-'*3)
-    for e in num:
-        print(f'{e}' , end=' ')
-        sleep(0.8)
-        if e == 0:
-            bigger = e
+    print('-->'* 4, "SCANNING NUMBERS", '<--'*4)
+    for each in num:
+        print(f'{each} ', end=" ")
+        sleep(0.5)
+        if each == 0:
+            bigger = each
         else:
-            if e > bigger:
-                bigger = e
-        cont += 1 
-    print(f'\nThe Biggest Number of this list is : {bigger}')
-        
-        
-bigger(6,9,13,2,8,14,31,2,5,22)
-bigger(9,15,3,21,4,17,1,12)
-bigger(4,8,12,10,7,5,19)
-bigger(5,15,29,46,33)
+            if each > bigger:
+                bigger = each
+        cont += 1
+    print(f'\nBetween {cont} numbers, The Biggest is = {bigger}')
+    print('-'* 42)
+    sleep(1.2)
+
+
+bigger(5, 8, 11, 15, 2, 6, 1, 9, 14, randint(1,35))
+bigger(19, 16, 11, 17, 3, 7, 4, randint(5,29))
+bigger(21, 13, 6, 7, 11, 8, randint(11,22))
+bigger(102,95,147,56, randint(39, 150))
+bigger(34, 49,28, randint(1,55))
