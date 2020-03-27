7a7dd5bdd854435de50735723feddcb2f8dddca4
Willian Viana <46612336+WillV9@users.noreply.github.com>
2020-03-26 23:02:14 -0700

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -1,19 +1,30 @@
-#           DESAFIO 101
-# Make a programa that has a FUNCTION CALLED vote() , it will receive as a PARAMETER the year of birthday of a person
-# Returning than a LITERAL VALUE ,indicating if this person right of vote is [NOT POSSIBLE / OPTIONAL / MANDATORY]
+#               DESAFIO 102
+#   MAKE A PROGRAM THAT HAS A FUNCTION CALLED fatorial() receiving TWO PARAMETERS:
+# The first one  indicates de number to calculate and the Second one called as SHOW will be a value logic
+# optional) indicating if will showing or not in the screen the process of calculing the fatorial
 
 
-def vote(year):
-    from datetime import date
-    act = date.today().year
-    age = act - ybday
-    if age < 16:
-        return f"You're {age} Years old and You can't Vote yet..."
-    elif 16 >= age < 18 or age >= 65:
-        return f"You're {age} Years old and Vote is Optional..."
-    else:
-        return f"You're {age} Years old and Vote is MANDATORY!"
+def fatorial(num, show=False):
+    '''
+    ~~> Function to calculate a number fatorial.
+    :param num: For the number
+    :param show: To show the complete fatorial progress
+    :return: the fatorial result
+    '''
+    f = 1
+    print(':-:' * 15)
+    for each in range(num, 0 , -1):
+        if show:
+            print(each, end='')
+            if each > 1:
+                print(f'x ', end='')
+            else:
+                print(f' = ', end='')
+        f *= each
+    return f
+print(':-:'* 15)
 
 
-ybday = int(input('What is your Year of Birthday? '))
-print(vote(ybday))
+
+num = int(input('Insert A number to Get their Fatorial: '))
+print(fatorial(num , show=True))
