474e1034c7e4395661052c5c614abba1e0354d3d
Willian Viana <46612336+WillV9@users.noreply.github.com>
2020-03-29 23:58:27 -0700

    Create ex21.py

---
+++ ex21.py
@@ -0,0 +1,15 @@
+# For the functions of doc Multi.py
+
+def fatorial(n):
+    f = 1
+    for e in range(1, n+1):
+        f *= e
+    return f
+
+
+def doble(n):
+    return n * 2
+
+
+def triple(n):
+    return n * 3
